ea87d3611f9768350d2516d78f2e49b45cdae636
S-M-Vaishnavi <smvaishnavi23@gmail.com>
2024-01-08 18:44:04 +0530

    name changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const NavBar = (props) => {
     
     return ( <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-    <a className="navbar-brand" href="#">JVLcode</a>
+    <a className="navbar-brand" href="#">CodeWithUs</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
     </button>
